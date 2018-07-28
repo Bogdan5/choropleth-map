3039f7c7c83361e9f29dcba5081fd2287d5ad6bd
Bogdan5 <bogdan5x@gmail.com>
2018-07-27 20:13:50 -0400

    Comment

--- JSFile.js
+++ JSFile.js
@@ -28,26 +28,41 @@ const drawMap = (data) => {
     .attr('class', 'containerSVG');
 
   // create a container for counties
-  let counties = svg.append('g')
-      .attr('id', 'counties')
-      .attr('class', 'Blues');
+  // let counties = svg.append('g')
+  //     .attr('id', 'counties')
+  //     .attr('class', 'Blues');
 
   // create a container for states
   let states = svg.append('g')
     .attr('id', 'states');
 
+  // create paths for each state using the json data
+  // and the geo path generator to draw the shapes
+  states.selectAll("path")
+  .datum(topojson.feature(data, data.objects.states))
+  .attr("d", path);
+
   // create paths for each county using the json data
   // and the geo path generator to draw the shapes
-  counties.selectAll('path')
-      .data(json.features)
-      .enter().append('path')
-      .attr('class', data ? quantize : null)
-      .attr('d', path);
+  // counties.selectAll('path')
+  //     .data()
+  //     .enter().append('path')
+  //     .attr('class', data ? quantize : null)
+  //     .attr('d', path);
+
+  console.log('states', data.objects.states);
 
   // create paths for each state using the json data
   // and the geo path generator to draw the shapes
   states.selectAll('path')
-    .data(json.features)
+    .data(data.objects.states)
     .enter().append('path')
     .attr('d', path);
+
+  // quantize function takes a data point and returns a number
+  // between 0 and 8, to indicate intensity, the prepends a 'q'
+  // and appends '-9'
+  function quantize(d) {
+    return "q" + Math.min(8, ~~(data[d.id] * 9 / 12)) + "-9";
+  }
 };
